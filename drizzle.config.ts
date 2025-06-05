b0deceae523d9dc67a8e0e71d287cacdd3a4a27e
Siddhesh Jadhav <119682823+siddhesh128@users.noreply.github.com>
2025-06-05 19:55:10 +0530

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,11 +2,3 @@ import type { Config } from "drizzle-kit";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-export default {
-  schema: "./lib/db/schema.ts",
-  out: "./lib/db/migrations",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: process.env.DATABASE_URL_UNPOOLED || "postgresql://neondb_owner:npg_Lrt7s3fGklav@ep-icy-fog-a11jc1un.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
-  },
-} as Config;
